4b762ac44ebb907e3149927d583e3f5ed9e8f4b0
Steve Hoffman <hoffman@middlebury.edu>
2024-08-11 15:27:51 -0500

    First commit of Script_R4DS_Ch1

---
+++ Script_R4DS_Ch1.R
@@ -0,0 +1,14 @@
+# SCRIPT CORRESPONDING TO CHAPTER 1: Data visualization IN R4DS
+
+# 1.1.1 Prerequisites --------------------------------------------------------- 
+
+# This chapter focuses on ggplot2, one of the core packages in the tidyverse. 
+# To access the datasets, help pages, and functions used in this chapter, load the tidyverse by running:
+library(tidyverse)
+
+# We will load the ggthemes package, which offers a colorblind safe color palette
+library(ggthemes)
+
+# And we will grab our play dataset from Google Sheets
+# So, load the package called googlesheets4
+library(googlesheets4)
